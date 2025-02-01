058bdc667d1ede8d115501642065c7f13726337d
Glebson Santos <glebsonsantos@outlook.pt>
2025-02-01 16:50:33 -0300

    feat: entity information

---
+++ src/information/entities/information.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('information')
+export class Information {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+    @Column({ name: 'title', nullable: false })
+    profession: string
+    @Column({ name: 'name', nullable: false })
+    name: string;
+    @Column({ name: 'description', nullable: false })
+    description: string;
+    @Column({ name: 'imageUrl', nullable: true })
+    imageUrl: string;
+    @Column({ name: 'linkedlnUrl', nullable: true })
+    linkedlnUrl: string;
+    @Column({ name: 'githubUrl', nullable: true })
+    githubUrl: string;
+    @Column({ name: 'instagramUrl', nullable: true })
+    instagramUrl: string;
+    @Column({ name: 'facebookUrl', nullable: true })
+    facebookUrl: string;
+    @Column({ name: 'phone', nullable: true })
+    whatsapp: string;
+    @Column({ name: 'email', nullable: false })
+    email: string;
+}
