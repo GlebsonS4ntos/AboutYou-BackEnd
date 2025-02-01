a19ca4f0a5ba244aec1dbb8c56c81c2bd551a3f0
Glebson Santos <glebsonsantos@outlook.pt>
2025-02-01 15:37:55 -0300

    feat: Add Information module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InformationModule } from './information/information.module';
+import { CloudinaryService } from './utils/cloudinary.service';
 
 @Module({
-  imports: [],
+  imports: [InformationModule],
   controllers: [AppController],
   providers: [AppService],
 })
